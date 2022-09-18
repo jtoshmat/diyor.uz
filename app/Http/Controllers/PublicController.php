8fb7b70e4d9a19c112ec4d4abd7dae5fb0b91f85
Jon Toshmatov <info@myitedu.us>
2022-09-18 10:02:45 -0400

    service page is created

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,4 +9,7 @@ class PublicController extends Controller
     public function index(){
         return view('index');
     }
+    public function service(){
+        return view('service');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\PublicController::class, 'index'])->name('index');
+Route::get('/service', [\App\Http\Controllers\PublicController::class, 'service'])->name('service');
 
 
 
